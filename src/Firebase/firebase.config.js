1e674b80fac3727888ccc76f2608db537980ba54
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-04-29 01:11:03 +0600

    initial route and firebase setUp added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChWOJtsBDSY6408UAoK1GqDm4QBGSWHng",
+  authDomain: "watch-flashsale-pro.firebaseapp.com",
+  projectId: "watch-flashsale-pro",
+  storageBucket: "watch-flashsale-pro.firebasestorage.app",
+  messagingSenderId: "301784899096",
+  appId: "1:301784899096:web:abfb9725769aa237802d6d",
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/routes/routers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hii</p>,
+  },
+]);
+
+export default routers;
